54b9fd58f86973048ab06ef2ca2260d6c2995345
admin <admin@ADMINISTRATOR>
2016-09-24 12:37:39 +0800

    create

---
+++ XAYEXCELS/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace XAYEXCELS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runtsm = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runtsm1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputtsm = new System.Windows.Forms.ToolStripMenuItem();
+            this.outputtsm = new System.Windows.Forms.ToolStripMenuItem();
+            this.closetsm = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CANDA";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runtsm,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            this.contextMenuStrip1.Text = "菜单";
+            // 
+            // runtsm
+            // 
+            this.runtsm.Name = "runtsm";
+            this.runtsm.Size = new System.Drawing.Size(100, 22);
+            this.runtsm.Text = "运行";
+            this.runtsm.Click += new System.EventHandler(this.runtsm_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(100, 22);
+            this.toolStripMenuItem3.Text = "退出";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(279, 176);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(334, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 204);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日志";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(657, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runtsm1,
+            this.inputtsm,
+            this.outputtsm,
+            this.closetsm});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // runtsm1
+            // 
+            this.runtsm1.Name = "runtsm1";
+            this.runtsm1.Size = new System.Drawing.Size(148, 22);
+            this.runtsm1.Text = "运行";
+            this.runtsm1.Click += new System.EventHandler(this.runtsm_Click);
+            // 
+            // inputtsm
+            // 
+            this.inputtsm.Name = "inputtsm";
+            this.inputtsm.Size = new System.Drawing.Size(148, 22);
+            this.inputtsm.Text = "输入路径选择";
+            this.inputtsm.Click += new System.EventHandler(this.inputtsm_Click);
+            // 
+            // outputtsm
+            // 
+            this.outputtsm.Name = "outputtsm";
+            this.outputtsm.Size = new System.Drawing.Size(148, 22);
+            this.outputtsm.Text = "输出路径选择";
+            this.outputtsm.Click += new System.EventHandler(this.outputtsm_Click);
+            // 
+            // closetsm
+            // 
+            this.closetsm.Name = "closetsm";
+            this.closetsm.Size = new System.Drawing.Size(148, 22);
+            this.closetsm.Text = "退出";
+            this.closetsm.Click += new System.EventHandler(this.closetsm_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.关于ToolStripMenuItem.Text = "关于";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 358);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem runtsm;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inputtsm;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outputtsm;
+        private System.Windows.Forms.ToolStripMenuItem closetsm;
+        private System.Windows.Forms.ToolStripMenuItem runtsm1;
+    }
+}
+
---
+++ XAYEXCELS/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace XAYEXCELS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IWorkbook workbook;
+        public System.Data.DataTable ImportExcelFile()
+        {
+            #region
+            string[] targetfile = Directory.GetFiles("F:\\代理");
+            DataTable drtable = new System.Data.DataTable();
+            for (int k = 0; k < targetfile.Length; k++)
+            {
+            try
+            {
+                string filePath = targetfile[k];
+                using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    //file.Position = 0;
+                    if (filePath.EndsWith(".xls"))
+                    {
+                        workbook = new HSSFWorkbook(file);
+                    }
+                    else /*if (filePath.EndsWith(".xlsx"))*/
+                    {
+                        workbook = new XSSFWorkbook(file);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+                ISheet sheet = workbook.GetSheetAt(0);
+                IRow headerRow = sheet.GetRow(0);
+                int cellCount = 18;
+                int rowCount = sheet.LastRowNum;
+                if (k == 0)
+                {             
+                    for (int i = headerRow.FirstCellNum; i < cellCount; i++)
+                    {
+                        bool samecol = drtable.Columns.Contains(headerRow.GetCell(i).ToString());
+                        if (!samecol)
+                        {
+                            DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
+                            drtable.Columns.Add(column);
+                        }
+                    }
+                }
+
+                for (int i = (sheet.FirstRowNum + 2); i <= rowCount; i++)
+                {
+                    IRow row = sheet.GetRow(i);
+                    DataRow dataRow = drtable.NewRow();
+
+                    if (row != null)
+                    {
+                        for (int j = row.FirstCellNum; j < cellCount; j++)
+                        {
+                            if (row.GetCell(j) != null)
+                            {
+                                dataRow[j] = row.GetCell(j);
+                            }
+
+                        }
+                    }
+
+                    drtable.Rows.Add(dataRow);
+                }
+            }
+ #endregion           
+            return drtable;
+        }
+        public static void ExportEasy(DataTable dtSource)
+        {
+            XSSFWorkbook workbook1 = new XSSFWorkbook();
+            ISheet sheet = workbook1.CreateSheet();           
+            IRow dataRow = sheet.CreateRow(0);
+            ICellStyle style = workbook1.CreateCellStyle();
+            ICellStyle styleh = workbook1.CreateCellStyle();
+            IFont font = workbook1.CreateFont();
+            dataRow.Height = 28 * 20;     
+            style.BorderBottom = NPOI.SS.UserModel.BorderStyle.Thin;
+            style.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+            style.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+            style.BorderTop = NPOI.SS.UserModel.BorderStyle.Thin;             
+            font.FontName = "宋体";           
+            font.FontHeightInPoints = 10;
+            style.SetFont(font);
+            styleh.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Center;
+            styleh.VerticalAlignment = VerticalAlignment.Center;
+            styleh.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.LightTurquoise.Index;
+            styleh.FillPattern = FillPattern.SolidForeground;
+            styleh.FillBackgroundColor = NPOI.HSSF.Util.HSSFColor.LightTurquoise.Index;
+            //font.IsBold =true;
+            styleh.SetFont(font);
+            string strFileName = "F:\\代理\\生成\\" + DateTime.Now.ToString("yyMMdd")+".xlsx";
+            foreach (DataColumn column in dtSource.Columns)
+            {
+                dataRow.CreateCell(column.Ordinal).SetCellValue(column.ColumnName);
+                dataRow.Cells[column.Ordinal].CellStyle = styleh;
+            }
+
+
+            //填充内容
+            for (int i = 0; i < dtSource.Rows.Count; i++)
+            {
+                dataRow = sheet.CreateRow(i + 1);
+                for (int j = 0; j < dtSource.Columns.Count; j++)
+                {
+                    dataRow.CreateCell(j).SetCellValue(dtSource.Rows[i][j].ToString());
+                    dataRow.Cells[j].CellStyle = style;
+                   
+                }
+              
+            }
+
+           
+            //保存
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(strFileName, FileMode.Create, FileAccess.Write))
+                {
+                    workbook1.Write(fs);
+                }
+            }
+            
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+        }     
+
+       
+        //protected override void OnClosing(CancelEventArgs e)
+        //{
+        //    this.ShowInTaskbar = false;
+        //    this.WindowState = FormWindowState.Minimized;
+        //    e.Cancel = true;
+        //}
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+      
+
+        private void inputtsm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputtsm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closetsm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void runtsm_Click(object sender, EventArgs e)
+        {
+           
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            string[] a = System.IO.Directory.GetFiles("F:\\代理");          
+             DataTable dt= ImportExcelFile();            
+            dt.DefaultView.Sort = "业务员,产品名称,数量";
+            dt = dt.DefaultView.ToTable();
+            ExportEasy(dt);
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            double seconds = timeSpan.TotalSeconds;
+            textBox1.Text = "共合并" + dt.Rows.Count + "行"+"，耗时"+seconds+"秒";
+
+
+        }
+
+    }
+   
+  }
